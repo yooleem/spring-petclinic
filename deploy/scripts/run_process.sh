0c63dabb8b789866bd3d6e127b9eaf66f287b694
yooleem <116706590+yooleem@users.noreply.github.com>
2023-02-14 15:21:09 +0900

    Create run_process.sh

---
+++ deploy/scripts/run_process.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REGION="ap-northeast-2"
+AWS_ACCOUNT_ID="257307634175"
+DOCKER_IMAGE_NAME="std00-spring-petclinic"        
+ECR_REPOSITORY="${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-2.amazonaws.com"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/${DOCKER_IMAGE_NAME}"
+ECR_DOCKER_TAG="1.0"
+
+aws ecr get-login-password --region ${REGION} \
+  | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
+
+export IMAGE=${ECR_DOCKER_IMAGE};
+export TAG=${ECR_DOCKER_TAG};
+docker-compose -f /home/ubuntu/docker-compose.yml up -d --build;
